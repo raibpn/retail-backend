9cc4d51480787a1343d0af6e8d437d00cbd26d3a
raibpn <raibpn@users.noreply.github.com>
2023-02-04 23:34:21 +0100

    graphql dependencies

--- src/product/controllers/product/product.controller.ts
+++ src/product/controllers/product/product.controller.ts
@@ -1,10 +1,24 @@
 import { Controller } from '@nestjs/common';
-import { Get } from '@nestjs/common/decorators';
+import { Body, Get, Param, Post, Req, Res } from '@nestjs/common/decorators';
+import { Request, Response } from 'express';
+import { CreateProductDto } from 'src/product/dtos/CreateProduct.dto';
 
 @Controller('product')
 export class ProductController {
   @Get()
   getProduct() {
     return { name: 'pant', price: '200dkk' };
+  }
+
+  @Post()
+  createProduct(@Body() productData: CreateProductDto) {
+    console.log(productData);
+    return {};
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+    console.log(id);
+    return id;
   }
 }
