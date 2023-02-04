6a1d70a005dca758341577ddc16b9436876dc103
raibpn <raibpn@users.noreply.github.com>
2023-02-04 23:45:36 +0100

    model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 // import { DatabaseModule } from './database/database.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/product/models/product.ts
@@ -0,0 +1,4 @@
+export class Product {
+  title: string;
+  price: number;
+}
