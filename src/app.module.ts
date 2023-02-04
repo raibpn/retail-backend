aceae000820794509847d6089c1fcaa59fa8d01a
raibpn <raibpn@users.noreply.github.com>
2023-02-04 22:54:20 +0100

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ProductModule } from './product/product.module';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
