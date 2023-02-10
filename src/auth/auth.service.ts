bbf92451401934e09153c3e1c28e7bddc031dc77
raibpn <raibpn@users.noreply.github.com>
2023-02-10 20:42:16 +0100

    token and strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,10 +14,13 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
   constructor(
-    private prisma: PrismaService, // private jwt: JwtService, // private config: ConfigService,
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
   ) {}
 
   async signup(dto: AuthDto) {
+    console.log('signup');
     // generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
@@ -32,8 +35,7 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-      return user;
+      return this.singToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -49,6 +51,7 @@ export class AuthService {
   //SIGNIN
   async signin(dto: signInDto) {
     //find the user by email
+    console.log('signin');
     const user = await this.prisma.user.findUnique({
       where: {
         email: dto.email,
@@ -60,27 +63,24 @@ export class AuthService {
     const pwMathces = await argon.verify(user.hash, dto.password);
     if (!pwMathces) throw new ForbiddenException('Credential incorrect');
 
-    delete user.hash;
-    return user;
+    return this.singToken(user.id, user.email);
   }
 
-  // async signToken(
-  //   userId: string,
-  //   email: string,
-  // ): Promise<{ access_token: string }> {
-  //   const payload = {
-  //     sub: userId,
-  //     email,
-  //   };
-  //   const secret = this.config.get('JWT_SECRET');
-
-  //   const token = await this.jwt.signAsync(payload, {
-  //     expiresIn: '15m',
-  //     secret: secret,
-  //   });
-
-  //   return {
-  //     access_token: token,
-  //   };
-  // }
+  async singToken(
+    userId: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      access_token: token,
+    };
+  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe() {
+    return 'this is me';
+  }
+}
