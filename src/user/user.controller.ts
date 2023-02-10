24b041fb74246e8c285821bc71fa3064fbe543cd
raibpn <raibpn@users.noreply.github.com>
2023-02-10 22:07:30 +0100

    cleaning decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, UseGuards, Get, Req } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
